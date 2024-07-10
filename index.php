658d91c3dedc318c3acaa1523ff7c5b57b07f920
Rocky Latchman <kidmongoose@gmail.com>
2024-07-09 23:45:42 -0700

    CSS and HTML for homepage

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php include 'includes/header.php'; ?>
-<h1>Hello</h1>
-<a href="/event_machine/events/">events</a>
+<header>
+    <h2><a href="/event_machine/">Event Machine</a></h2>
+    <nav>
+      <div><a href="signin/">Sign in</a></div>
+    </nav>
+</header>
+<main>
+    <div>
+        <h1>
+          Share your events or 
+          find cool things going 
+          on around you  
+        </h1>
+        <a href="get-started/">Get started</a>
+    </div>
+</main>
 <?php include 'includes/footer.php'; ?>
