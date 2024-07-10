7a727c23997d716033768ece39b6031eab9f1374
Rocky Latchman <kidmongoose@gmail.com>
2024-07-09 21:48:12 -0700

    Project structure created

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Event Machine</title>
+    <link rel="stylesheet" href="https://use.typekit.net/jhj8lgl.css">
+    <link rel="stylesheet" href="/event_machine/css/main.css">
+   </head>
+   <body>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'includes/header.php'; ?>
+<h1>Hello</h1>
+<a href="/event_machine/events/">events</a>
+<?php include 'includes/footer.php'; ?>
