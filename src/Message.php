0948176545b28b47f1612653e54d72c988e8e628
Rocky Latchman <kidmongoose@gmail.com>
2024-07-20 22:42:10 -0700

    send_message method added to Message class

--- src/Message.php
+++ src/Message.php
@@ -20,7 +20,7 @@ class Message {
    
 
    function get_message($message_id){
-       $mysqli = new mysqli();
+       $mysqli = new mysqli($this->db->host, $this->db->username, $this->db->database);
        $stmt = $mysqli->prepare("SELECT * from message WHERE message_id=?");
        $stmt->bind_param('s', $message_id);
        $stmt->execute();
@@ -28,11 +28,17 @@ class Message {
    }
    
    function send_message($message_data){
-
+       $mysqli = new mysqli($this->db->host, $this->db->username, $this->db->database);
+       $stmt = $mysqli->prepare("INSERT INTO message (subject, message, message_date, sender_id, recipient_id) VALUES(?, ?, ?, ?, ?)");  
+       $formatted_date = new DateTime('now');
+       $formatted_message_date = $formatted_date->format('Y-m-d');
+       $stmt->bind_param('sssss', $message_data['subject'], $message_data['message'], $formatted_message_date, $message_data['sender_id'], $message_data['recipient_id']);
+       $stmt->execute();
+       $stmt->close();
    }
 
    function delete_message($message_id){
-
+       
    }
 
    function delete_all_messages($user_id){
