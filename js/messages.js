6990e52b4e26d9ac0877f198b599af09999747e1
Rocky Latchman <kidmongoose@gmail.com>
2024-07-23 13:38:06 -0700

    Added createWindow function in js for messages page

--- js/messages.js
+++ js/messages.js
@@ -4,3 +4,18 @@ const friendList = document.querySelector('#friend-list');
 const messages = document.querySelector('#em-message');
 const overlay = document.createElement('div');
 overlay.setAttribute('id', 'overlay');
+
+function createWindow() {
+    const windowDiv = document.createElement('div');
+    const closeButton = document.createElement('a');
+    const closeButtonText = document.createTextNode('X')
+    closeButton.setAttribute('id', 'close');
+    closeButton.setAttribute('href', '');
+    closeButton.appendChild(closeButtonText);
+    windowDiv.appendChild(closeButton);
+    closeButton.addEventListener('click', (e) => {
+       e.preventDefault();
+       closeWindow();
+    })
+    return windowDiv;   
+ }
