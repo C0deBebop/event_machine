bbe50472f8489630a4287f5f139787ed3cd5e546
Rocky Latchman <kidmongoose@gmail.com>
2024-07-23 13:45:19 -0700

    messageWindow js function added for messages page

--- js/messages.js
+++ js/messages.js
@@ -5,6 +5,23 @@ const messages = document.querySelector('#em-message');
 const overlay = document.createElement('div');
 overlay.setAttribute('id', 'overlay');
 
+
+function messageWindow(){
+    const windowDiv = createWindow();
+    const heading = document.createElement('h4');
+    const headingText = document.createTextNode('Message');
+    windowDiv.getAttribute('id', 'message-window');
+    heading.appendChild(headingText);
+    windowDiv.appendChild(heading);
+    overlay.appendChild(windowDiv);
+    document.querySelector('body').appendChild(overlay);
+}
+
+function closeWindow(){
+    document.querySelector('#overlay').remove();
+}
+
+
 function createWindow() {
     const windowDiv = document.createElement('div');
     const closeButton = document.createElement('a');
